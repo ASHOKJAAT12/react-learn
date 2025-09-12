a69e80187b8542bb645aa6c4bc694e0bdfcbfbf5
ASHOKJAAT12 <ajat39963@gmail.com>
2025-09-12 23:37:43 +0530

    08mcq

---
+++ 08mcq/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <section className='w-full h-screen bg-[#1a1a1a] flex text-white justify-center items-center '>
+      <div className='bg-[#afafaf]'>
+        <h1>which is the world fastes language</h1>
+
+      </div>
+    </section>
+  )
+}
+
+export default App
