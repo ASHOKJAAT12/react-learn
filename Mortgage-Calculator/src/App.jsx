25e1bf2eeafd1fc2febfbe153e627ff8c0bec2a5
ASHOKJAAT12 <ajat39963@gmail.com>
2025-09-14 23:44:35 +0530

    mortgage calculator pending

---
+++ Mortgage-Calculator/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <section className='bg-[#1a1a1a]'>
+      
+    </section>
+  )
+}
+
+export default App
