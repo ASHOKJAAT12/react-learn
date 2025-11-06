9dc8a000eb84315e3ea75fa2793817242d91183b
ASHOKJAAT12 <ajat39963@gmail.com>
2025-11-06 21:17:20 +0530

    create 14 guess number

--- 13ecommerce/src/App.jsx
+++ 13ecommerce/src/App.jsx
@@ -2,8 +2,15 @@
 function App() {
 
   return (
-    <section>
-      <h1 className=" text-black text-4xl">Welcome to the E-commerce App</h1>
+    <section className="w-full h-screen flex justify-center items-center bg-[#333]">
+      <section className="bg-[#999] h-[400px] flex justify-center pt-10 w-140 rounded-2xl">
+        <h1 className="text-[#308D46] font-bold ">Welcome to the E-commerce App</h1>
+        <div>
+          <li>
+
+          </li>
+        </div>
+      </section>
     </section>
   )
 }
